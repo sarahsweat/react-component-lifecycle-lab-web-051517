11ccf9ad9a1c746f487678b1cef180a2bdde7f41
sarahsweat <sarahsweat@gmail.com>
2017-07-19 15:06:27 -0400

    Done.

--- src/components/TweetWall.js
+++ src/components/TweetWall.js
@@ -4,15 +4,29 @@ import Tweet from './Tweet';
 class TweetWall extends React.Component {
   constructor(props) {
     super(props);
-    
+
     this.state = {
       tweets: []
     };
   }
 
   // TODO: componentWillMount()
+
+  componentWillMount() {
+    this.setState({ tweets: this.props.newTweets});
+  }
   // TODO: shouldComponentUpdate()
   // TODO: componentWillReceiveProps()
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      tweets: [...nextProps.newTweets, ...this.state.tweets]
+    })
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+  return (this.props.tweets !== nextProps.tweets);
+}
 
   render() {
     const tweets = this.state.tweets.map((tweet, index) => <Tweet text={tweet.text} key={index} />);
